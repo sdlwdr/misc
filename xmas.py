bbbadc1a3438ce16e94ac8618ce7456b66d1952d
Samuel DeLaughter <sdlwdr@gmail.com>
2014-12-06 12:00:00 -0500

    Update xmas.py

--- xmas.py
+++ xmas.py
@@ -1,7 +1,7 @@
 #Author: Samuel DeLaughter
 
 gifts=['A Partridge in a Pear Tree', 'Two Turtle Doves, and', 'Three French Hens', 'Four Calling Birds', 'Five Golden Rings', 'Six Geese-a-Laying', 'Seven Swans-a-Swimming', 'Eight Maids-a-Milking', 'Nine Ladies Dancing', 'Ten Lords-a-Leaping', 'Eleven Pipers Piping', 'Twelve Drummers Drumming']
-ordinal=['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th']
+ordinal=['st', 'nd', 'rd'] + ['th']*9
 
 for day in range(12):
   print('On the ' + str(day+1) + str(ordinal[day]) + ' day of Christmas, my true love sent to me...')
