f3c06bb22ec1b8147b23b0d82a3cd18412402a7e
Samuel DeLaughter <sdlwdr@gmail.com>
2015-03-25 15:43:27 -0400

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -6,32 +6,32 @@
 
 import sys
 
-MIN=1
-MAX=10
-if len(sys.argv) == 3:
+MIN = 1
+MAX = 10
+if(len(sys.argv) == 3):
 	MIN = int(sys.argv[1])
 	if MIN < 1:
 		MIN = 1
 	MAX = int(sys.argv[2])
 	if MAX < MIN:
-		MAX=MIN+1
+		MAX = MIN + 1
 
 def test(number):
 	n = number
 	counter = 0
-	while not (n == 1):
-		if n % 2 == 0:
+	while(not (n == 1)):
+		if((n % 2) == 0):
 			n = (n / 2)
 		else:
 			n = ((n * 3) + 1)
-		counter+=1
+		counter += 1
 	print ('Reached 1 from ' + str(number) + ' in ' + str(counter) +  ' steps')
 
 def main():
-	number=MIN
+	number = MIN
 	while number <= MAX:
 		test(number)
-		number+=1
+		number += 1
 	print('Done')
 
 if __name__ == '__main__':
